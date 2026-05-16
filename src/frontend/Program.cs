c44498691387a83333bf2d23832fa724628d092c
simplyBarbe <andrea.b-1999@libero.it>
2026-05-16 20:30:12 +0200

    removed boilerplate

--- src/frontend/Program.cs
+++ src/frontend/Program.cs
@@ -13,7 +13,6 @@ namespace frontend
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddMudServices();
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
